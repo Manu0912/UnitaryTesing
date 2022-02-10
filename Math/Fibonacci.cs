784507b13451aa8939721bfcc2580a21bd7b3c17
Emanuel Hugo Cacheda <emanuelcacheda@hotmail.com>
2022-02-10 11:38:06 -0700

    fibonacci test

---
+++ Math/Fibonacci.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math
+{
+    public class Fibonacci
+    {
+        public Fibonacci()
+        {
+                
+        }
+        public int GetFibonacciNumber(int n1)
+        {
+            if (n1 <= 2 && n1 > 0)
+                return 1;
+            else if (n1 == 0) return 0;
+            else
+                return GetFibonacciNumber(n1 - 1) + GetFibonacciNumber(n1 - 2);
+        }
+    }
+}
---
+++ UnitaryTesting/FibonacciTest.cs
@@ -0,0 +1,19 @@
+﻿using Math;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+namespace UnitaryTesting
+{
+    [TestClass]
+    public class FibonacciTest
+    {
+        [TestMethod]
+        public void TestFibonacciNumber()
+        {
+            Fibonacci fib = new();
+            var res = fib.GetFibonacciNumber(10);
+            Assert.IsNotNull(res);
+            Assert.AreEqual(55, res);
+            
+        }
+    }
+}
