e24fbcc176f9ea476028ddfd37c45d11251e1b06
Emanuel Hugo Cacheda <emanuelcacheda@hotmail.com>
2022-02-18 14:10:57 -0700

    videoService exercise

---
+++ BookingHelper/Mocking/FileReader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcercisesChapterSix.Mocking
+{
+    public interface IFileReader
+    {
+        string Read(string fileName);
+    }
+
+    public class MockFileReader : IFileReader
+    {
+        public string Read(string fileName)
+        {
+            return "";
+        }
+    }
+
+    public class FileReader: IFileReader
+    {
+        public string Read(string fileName)
+        {
+            return File.ReadAllText(fileName);
+        }
+    }
+}
