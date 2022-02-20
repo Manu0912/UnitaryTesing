a51440fcb61a55bc3732e86215c1aede73e72021
Emanuel Hugo Cacheda <emanuelcacheda@hotmail.com>
2022-02-20 03:07:22 -0700

    BookingHelper Exercise

---
+++ BookingHelperProject/Booking.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingHelperProject
+{
+    public class Booking
+    {
+        public string Status { get; set; }
+        public int Id { get; set; }
+        public DateTime ArrivalDate { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public string Reference { get; set; }
+    }
+    
+}
---
+++ BookingHelperProject/BookingHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingHelperProject
+{
+    public static class BookingHelper
+    {
+        public static string OverlappingBookingsExist(Booking currentBooking, IBookingRepository bookingRepository)
+        {
+            if (currentBooking.Status == "Cancelled")
+                return string.Empty;
+
+            var activeBookings = bookingRepository.GetActiveBookings(currentBooking.Id);
+
+            var overlappingBooking =
+                activeBookings.FirstOrDefault(
+                    ab =>
+                        currentBooking.ArrivalDate < ab.DepartureDate
+                        && ab.ArrivalDate < currentBooking.DepartureDate);
+
+            return overlappingBooking == null ? string.Empty : overlappingBooking.Reference;
+        }
+    }
+}
